059f53449c5e466ce8159abff7be1d344ff77b81
birdthief <jdlaudio@gmail.com>
2026-07-09 21:25:55 -0400

    HEAD>>>REMOVAL

--- services/API.js
+++ services/API.js
@@ -1,9 +1,5 @@
 const API = {
-<<<<<<< HEAD
   url: new URL("../data/portfolio.json", import.meta.url).href,
-=======
-  url: "/data/portfolio.json",
->>>>>>> refs/remotes/origin/main
   fetchData: async () => {
     const result = await fetch(API.url);
     return await result.json();
