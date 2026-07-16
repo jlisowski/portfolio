8f8061fc146a0a32695fe3a9a89ae563aa7fde02
Birdthief <jdlaudio@gmail.com>
2026-07-15 22:42:48 -0400

    Bugfix: Express server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
-const history = require('connect-history-api-fallback');
-
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-// 1. SPA routing fallback: Redirects non-file requests to index.html
-app.use(history());
+const PORT = process.env.PORT || 3000;
 
 // 2. Serve static files (HTML, CSS, JS) from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 3. Start the Express server
+//fallback to index.html for SPA routing
+app.get('/*splat', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
-  console.log(`[Express] Server running on http://localhost:${PORT}`);
+  console.log(`Express server listening on http://localhost:${PORT}`);
 });
