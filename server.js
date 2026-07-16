8230b135a6a49eaaa8d112cbc26994a3a2ba4998
Birdthief <jdlaudio@gmail.com>
2026-07-16 12:58:43 -0400

    Another port fix + Font

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // 2. Serve static files (HTML, CSS, JS) from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
